bdfe3d95e94c19665f3bfc034aada1c880eb11eb
Akancha Agarwal <47851459+akancha12@users.noreply.github.com>
2026-04-18 22:36:32 +0530

    Add: asteroid-collision - submission-2

---
+++ Data Structures & Algorithms/asteroid-collision/submission-2.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> currentState = new Stack<>();
+        for (int asteroid : asteroids) {
+            if (asteroid >= 0 || currentState.isEmpty()
+                || isPositive(currentState.peek()) == isPositive(asteroid)) {
+                currentState.push(asteroid);
+            } else {
+                while (!currentState.isEmpty() && currentState.peek() >= 0
+                    && currentState.peek() < Math.abs(asteroid)) {
+                    currentState.pop();
+                }
+                if (currentState.isEmpty()) {
+                    currentState.push(asteroid);
+                } else if (currentState.peek() == Math.abs(asteroid)) {
+                    currentState.pop();
+                } else if (currentState.peek() < 0) {
+                    currentState.push(asteroid);
+                }
+            }
+        }
+        int result[] = new int[currentState.size()];
+        for (int i = result.length - 1; i >= 0; i--) {
+            result[i] = currentState.pop();
+        }
+        return result;
+    }
+    public boolean isPositive(int n) {
+        return n >= 0;
+    }
+}
