9f38891597e850be2f6fc6ec1139c5e6664a6bff
Akancha Agarwal <47851459+akancha12@users.noreply.github.com>
2026-04-19 00:18:27 +0530

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.java
@@ -0,0 +1,37 @@
+class Solution {
+    class Car {
+        Integer speed;
+        Integer position;
+        double time;
+        Car(int speed, int position, int target) {
+            this.speed = speed;
+            this.position = position;
+            this.setTime(target);
+        }
+        public void setTime(int target) {
+            this.time = ((target - this.position) * 1.0) / this.speed;
+        }
+        public String toString() {
+            return this.position + " " + this.time;
+        }
+    }
+    public int carFleet(int target, int[] position, int[] speed) {
+        List<Car> cars = new ArrayList<>();
+        for (int i = 0; i < position.length; i++) {
+            cars.add(new Car(speed[i], position[i], target));
+        }
+        cars.sort(new Comparator<>() {
+            public int compare(Car c1, Car c2) {
+                return c1.position.compareTo(c2.position);
+            }
+        });
+        int result = 0;
+        Stack<Car> stack = new Stack<>();
+        for (int i = position.length - 1; i >= 0; i--) {
+            Car current = cars.get(i);
+            if (stack.isEmpty() || stack.peek().time < current.time)
+                stack.push(current);
+        }
+        return stack.size();
+    }
+}
