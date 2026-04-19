68c347cfa9ea4903b147801001dd13d2fe3e284d
Akancha Agarwal <47851459+akancha12@users.noreply.github.com>
2026-04-19 18:18:47 +0530

    Add: simplify-path - submission-8

---
+++ Data Structures & Algorithms/simplify-path/submission-8.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String simplifyPath(String path) {
+        StringBuilder result = new StringBuilder();
+        Stack<String> stack = new Stack<>();
+        for (String s : path.split("/")) {
+            if (s.length() == 0 || s.equals(".")) continue;
+            if (s.equals("..")) {
+                if (!stack.isEmpty()) stack.pop();
+            } else {
+                stack.push(s);
+            }
+        }
+        while (!stack.isEmpty()) {
+            result.insert(0, stack.pop());
+            result.insert(0, '/');
+        }
+        if (result.length() == 0)
+            result.append('/');
+        return result.toString();
+    }
+}
