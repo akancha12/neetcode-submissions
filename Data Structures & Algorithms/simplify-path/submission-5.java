2dd0e74f733cac2cd1ebe5f8c356c6821bd1b7ea
Akancha Agarwal <47851459+akancha12@users.noreply.github.com>
2026-04-19 18:12:14 +0530

    Add: simplify-path - submission-5

---
+++ Data Structures & Algorithms/simplify-path/submission-5.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String simplifyPath(String path) {
+        StringBuilder result = new StringBuilder();
+        StringBuilder temp = new StringBuilder();
+        Stack<String> stack = new Stack<>();
+        int dotCount = 0;
+        path = path + '/';
+        for (char c : path.toCharArray()) {
+            if (c == '/') {
+                System.out.println(stack);
+                if (temp.toString().equals("..")) {
+                    if (!stack.isEmpty())
+                        stack.pop();
+                } else if (temp.length() > 0 && !temp.toString().equals(".")) {
+                    stack.push(temp.toString());
+                }
+                temp = new StringBuilder();
+            } else {
+                temp.append(c);
+            }
+        }
+        while (!stack.isEmpty()) {
+            result.insert(0, stack.pop());
+            result.insert(0, '/');
+        }
+        if (result.length() == 0)
+            result.append('/');
+        return result.toString();
+    }
+}
